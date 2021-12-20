7ad165b3d56b240aa647bd70e689dc9cc862ded5
Michael Pagliaro <mhp5035@rit.edu>
2021-12-20 13:53:57 -0500

    Add project files.

---
+++ SharpEncrypt/AesCryptographyService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEncrypt
+{
+    public class AesCryptographyService
+    {
+        public static int NUM_BYTES = 16;
+        private readonly byte[] key = new byte[16];
+        private readonly byte[] iv = new byte[16] { 0x23, 0x1C, 0xCE, 0x8F, 0x1D, 0x05, 0x47, 0x53, 0x8E, 0x98, 0x1D, 0x3F, 0xBE, 0xF2, 0xA8, 0x9D };
+
+        public AesCryptographyService(string password)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                key = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public byte[] Encrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.KeySize = NUM_BYTES * 8;
+                aes.BlockSize = NUM_BYTES * 8;
+                aes.Padding = PaddingMode.Zeros;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                {
+                    return PerformCryptography(data, encryptor);
+                }
+            }
+        }
+
+        public byte[] Decrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.KeySize = NUM_BYTES * 8;
+                aes.BlockSize = NUM_BYTES * 8;
+                aes.Padding = PaddingMode.Zeros;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                {
+                    return PerformCryptography(data, decryptor);
+                }
+            }
+        }
+
+        private byte[] PerformCryptography(byte[] data, ICryptoTransform cryptoTransform)
+        {
+            using (var ms = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+
+                return ms.ToArray();
+            }
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            var encrypted = Encrypt(data, key, iv);
+            return encrypted;
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            var decrypted = Decrypt(data, key, iv);
+            return decrypted;
+        }
+    }
+}
---
+++ SharpEncrypt/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace SharpEncrypt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private byte[] loadedFile;
+        private string filename = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateUI();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // TODO: WORK WITH FOLDERS AND FILES
+                loadedFile = File.ReadAllBytes(openFileDialog.FileName);
+                filename = openFileDialog.FileName;
+                UpdateUI();
+            }
+        }
+
+        private void Reset()
+        {
+            loadedFile = null;
+            filename = "";
+            textboxPassword.Text = "";
+            progressBar.Value = progressBar.Minimum;
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            labelName.Content = "Selected: " + filename;
+            btnEncrypt.IsEnabled = filename.Length > 0;
+            btnDecrypt.IsEnabled = filename.Length > 0;
+        }
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if (textboxPassword.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show("A password is required.");
+                return;
+            }
+
+            var aes = new AesCryptographyService(textboxPassword.Text.Trim());
+            for (int i = 0; i < loadedFile.Length; i += AesCryptographyService.NUM_BYTES)
+            {
+                progressBar.Value = (i / loadedFile.Length) * 100;
+                byte[] current = new byte[AesCryptographyService.NUM_BYTES];
+                for (int j = 0; j < AesCryptographyService.NUM_BYTES; ++j)
+                {
+                    if (i + j < loadedFile.Length)
+                        current[j] = loadedFile[i + j];
+                    else
+                        current[j] = 0;
+                }
+                byte[] encrypted = aes.Encrypt(current);
+                for (int j = 0; j < AesCryptographyService.NUM_BYTES; ++j)
+                {
+                    if (i + j < loadedFile.Length)
+                        loadedFile[i + j] = encrypted[j];
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("WARNING: Final block is not " + AesCryptographyService.NUM_BYTES.ToString() + " bytes.");
+                        break;
+                    }
+                }
+            }
+
+            progressBar.Value = progressBar.Maximum;
+            File.WriteAllBytes(filename, loadedFile);
+            MessageBox.Show("File successfully encrypted.");
+            Reset();
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if (textboxPassword.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show("A password is required.");
+                return;
+            }
+
+            var aes = new AesCryptographyService(textboxPassword.Text.Trim());
+            for (int i = 0; i < loadedFile.Length; i += AesCryptographyService.NUM_BYTES)
+            {
+                progressBar.Value = (i / loadedFile.Length) * 100;
+                byte[] current = new byte[AesCryptographyService.NUM_BYTES];
+                for (int j = 0; j < AesCryptographyService.NUM_BYTES; ++j)
+                {
+                    if (i + j < loadedFile.Length)
+                        current[j] = loadedFile[i + j];
+                    else
+                        current[j] = 0;
+                }
+                byte[] encrypted = aes.Decrypt(current);
+                // LAST 16 BYTES GET CORRUPTED WHEN DECRYPTING IF IT IS NOT 16 TOTAL
+                for (int j = 0; j < AesCryptographyService.NUM_BYTES; ++j)
+                {
+                    if (i + j < loadedFile.Length)
+                        loadedFile[i + j] = encrypted[j];
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("WARNING: Final block is not " + AesCryptographyService.NUM_BYTES.ToString() + " bytes.");
+                        break;
+                    }
+                }
+            }
+
+            progressBar.Value = progressBar.Maximum;
+            File.WriteAllBytes(filename, loadedFile);
+            MessageBox.Show("File successfully decrypted.");
+            Reset();
+        }
+    }
+}
